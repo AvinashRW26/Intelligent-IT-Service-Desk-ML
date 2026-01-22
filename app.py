a7f1885f9e74b1bf103fe2376bb2d1b9a9158a67
Avinash Waghmare <75696666+AvinashRW26@users.noreply.github.com>
2026-01-22 14:03:16 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
@@ -7,13 +7,21 @@ app = Flask(__name__)
 with open("ticket_classifier.pkl", "rb") as f:
     model = pickle.load(f)
 
+# GUI Route
+@app.route("/", methods=["GET", "POST"])
+def home():
+    prediction = None
+    if request.method == "POST":
+        ticket_text = request.form["ticket_text"]
+        prediction = model.predict([ticket_text])[0]
+    return render_template("index.html", prediction=prediction)
+
+# API Route
 @app.route("/predict", methods=["POST"])
-def predict():
+def predict_api():
     data = request.get_json()
     ticket = data.get("ticket_text")
-
     prediction = model.predict([ticket])[0]
-
     return jsonify({
         "ticket_text": ticket,
         "predicted_category": prediction
